e9513573115782251cc3bbbd313e70f20f67cc8e
Karbyhdus <mad-hatter_@gmx.de>
2022-09-27 14:34:55 +0200

    Arduino Weihnacht Tag 1

--- Arduino/arduinoAdventskalender/tag1/src/main.cpp
+++ Arduino/arduinoAdventskalender/tag1/src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
 
+unsigned long timeout_start_time = 0;
+bool timer(unsigned long zeit);
+bool AN;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool zeit_abgelaufen = timer(500);
+
+  if (zeit_abgelaufen && !AN)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    AN = true;
+  }
+  else if (zeit_abgelaufen && AN)
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+    AN = false;
+  }
+}
+
+bool timer(unsigned long zeit)
+{
+  unsigned long currentMillis = millis();
+
+  if (timeout_start_time == 0)
+  {
+    timeout_start_time = currentMillis;
+  }
+
+  if (currentMillis > (timeout_start_time + zeit))
+  {
+    timeout_start_time = 0;
+    return true;
+  }
+  return false;
 }
