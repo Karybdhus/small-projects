522a9d75c3093eaf99683cc96d6a166da7c13151
Karbyhdus <mad-hatter_@gmx.de>
2023-07-31 20:38:00 +0200

    new project

---
+++ C++/Geldautomat/account.cpp
@@ -0,0 +1,29 @@
+#include "deklarationen.h"
+
+Account::Account(float bal, float cr, std::string last, std::string first)
+    : balance(bal), credit(cr), lastName(last), firstName(first)
+{
+}
+
+std::string getLastName()
+{
+    
+}
+
+std::string getFirstName()
+{
+
+}
+
+float getBalance()
+{
+
+}
+
+bool changeBalance()
+{
+}
+
+void changeCredit()
+{
+}
